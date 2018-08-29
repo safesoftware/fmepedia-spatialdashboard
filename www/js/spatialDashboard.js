3bd5a8bc71ce18ba522362c647202aa59e8d95d5
geratsafe <gerhard.fischl@safe.com>
2018-08-29 11:00:12 -0700

    switch map

--- www/js/spatialDashboard.js
+++ www/js/spatialDashboard.js
@@ -7,29 +7,9 @@ function initialize() {
   var shipcount = document.getElementById('shipcount');
   var buscount = document.getElementById('buscount');
   var planecount = document.getElementById('planecount');
-  
-	var myLatlng = new google.maps.LatLng(37.7850,-122.4183);
 
-	var myOptions = {
-		zoom: 11,
-		center: myLatlng,
-		mapTypeId: google.maps.MapTypeId.ROADMAP,
-		mapTypeControl: true,
-		mapTypeControlOptions: {
-			style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-			position: google.maps.ControlPosition.LEFT_BOTTOM
-		},
-
-	}
-
-	var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-	var iconroot = "http://demos.fmeserver.com/spatialdashboard/libs/";
-
-	// Storage for Markers
-	var markers = new Array();
-	var markersBus = new Array();
-	var markersCustom = new Array();
-	var markersPlane = new Array();
+L.mapbox.accessToken = 'pk.eyJ1IjoibmF0aGFuYXRzYWZlIiwiYSI6ImNqazRqN2VuazA0dHczcXAyYjkyeTczcnUifQ.ZcD7wuTSCbsLRb_Y-drHjg'
+	var map = new L.mapbox.map(document.getElementById("map"),'mapbox.streets').setView([37.78,-122.41],9);
 
 	// Storage for WebSocket connections
 	var ws;
@@ -40,7 +20,7 @@ function initialize() {
 	// Do we have web sockets?
 	if ("WebSocket" in window) {
 
-	
+
 		// ============= AIS ====================
 		ws = FMEServer.getWebSocketConnection("sd_ship");
 
@@ -62,54 +42,28 @@ function initialize() {
 			"class_special_desc": "Pusher/Tug "
 			}
 			*/
-
-      
       shipcount.textContent = parseInt(shipcount.textContent) + 1;
-      
+
 			var data = evt.data;
 			dataObj = JSON.parse(data);
-			// document.getElementById('container').innerHTML = dataObj['latitude'];
-			mmsi = dataObj.mmsi_number;
-			var point = new google.maps.LatLng(dataObj['latitude'],dataObj['longitude']);
-			// alert(markers[mmsi]);
 
-			var image = iconroot + "ship.png";
-			var ton = dataObj['tonnage'];
-			if (ton < 100) {
-				var imgScaledSize = new google.maps.Size(30,30);
-			}
-			else if (ton > 100 && ton < 10000) {
-				var imgScaledSize = new google.maps.Size(35,35);
-			}
-			else if (ton > 10000) {
-				var imgScaledSize = new google.maps.Size(50,50);
-			}
-			else {
-				var imgScaledSize = new google.maps.Size(30,30);
-			}
-
-			// var imgScaledSize = new google.maps.Size(20,20);
-			var markerImg = new google.maps.MarkerImage(image, null, null, null, imgScaledSize);
-			// var markerImg = new google.maps.MarkerImage(image, imgScaledSize , imgScaledOrigin, imgScaledAnchor, imgSize);
-      
-			if(markers[mmsi] == undefined) {
-
-				var title = "Vessel: " + dataObj['vessel_name'] + "\n Class: " + dataObj['class_special_desc'] + "\n Tonnage: " + dataObj['tonnage'];
-				// alert('a');
-				var marker = new google.maps.Marker({
-					position: point,
-					map: map,
-					title: title,
-					icon: markerImg
-				});
-				markers[mmsi] = marker;
-			}
-			else {
-				// alert('b');
-				markers[mmsi].setPosition(point);
-			}
-
-			// alert(data);
+			L.mapbox.featureLayer({
+		    	type: 'Feature',
+		    	geometry: {
+		        type: 'Point',
+		        coordinates: [
+		          dataObj['longitude'],
+		          dataObj['latitude']
+		        ]
+		    	},
+					properties: {
+				        title: dataObj['vessel_name'],
+				        description: dataObj['class_general_desc'],
+				        'marker-size': 'medium',
+				        'marker-color': '#4286f4',
+				        'marker-symbol': 'ferry'
+				    }
+			}).addTo(map);
 		};
 
 		// close
@@ -129,34 +83,27 @@ function initialize() {
 			"speed": "0.0"
 			}
 			*/
-      
       buscount.textContent = parseInt(buscount.textContent) + 1;
-      
+
 			var data = evt.data;
 			dataObj = JSON.parse(data);
 
-			bus_id = dataObj['bus_id'];
-			var point = new google.maps.LatLng(dataObj['latitude'],dataObj['longitude']);
-
-			var image = iconroot + "bus.png";
-			var imgScaledSize = new google.maps.Size(30,30);
-
-			var markerImg = new google.maps.MarkerImage(image, null, null, null, imgScaledSize);
-
-			if(markersBus[bus_id] == undefined) {
-				var title = "Bus: " + dataObj['bus_id'] + "\n Speed: " + dataObj['speed'] + " km/h";
-				var marker = new google.maps.Marker({
-					position: point,
-					map: map,
-					title: title,
-					icon: markerImg
-				});
-				markersBus[bus_id] = marker;
-			}
-			else {
-				markersBus[bus_id].setPosition(point);
-			}
-
+			L.mapbox.featureLayer({
+					type: 'Feature',
+					geometry: {
+						type: 'Point',
+						coordinates: [
+							dataObj['longitude'],
+							dataObj['latitude']
+						]
+				},
+				properties: {
+							title: dataObj['bus_id'],
+							'marker-size': 'medium',
+							'marker-color': '#23a046',
+							'marker-symbol': 'bus'
+					}
+		}).addTo(map);
 		};
 
 		// close
@@ -180,43 +127,34 @@ function initialize() {
 			"speed": "484.4"
 			}
 			*/
-
       planecount.textContent = parseInt(planecount.textContent) + 1;
-      
+
 			var data = evt.data;
 			dataObj = JSON.parse(data);
 
-			the_id = dataObj['id'];
-			var point = new google.maps.LatLng(dataObj['latitude'],dataObj['longitude']);
-
-			var image = iconroot + "plane.png";
-			var imgScaledSize = new google.maps.Size(30,30);
-
-			var markerImg = new google.maps.MarkerImage(image, null, null, null, imgScaledSize);
-
-			if(markersPlane[the_id] == undefined) {
-				var marker = new google.maps.Marker({
-					position: point,
-					map: map,
-					title: "Flight: " + dataObj['flight_id'] + "\n Aircraft: " + dataObj['aircraft_model'] + "\n Altitude: " + dataObj['altitude'] + " m\n Speed: " + dataObj['speed'] + " km/h",
-					icon: markerImg
-				});
-				markersPlane[the_id] = marker;
+	L.mapbox.featureLayer({
+			type: 'Feature',
+			geometry: {
+				type: 'Point',
+				coordinates: [
+					dataObj['longitude'],
+					dataObj['latitude']
+				]
+		},
+		properties: {
+					title: dataObj['aircraft_model'],
+					description: dataObj['flight_id'],
+					'marker-size': 'medium',
+					'marker-color': '#a03723',
+					'marker-symbol': 'airfield'
 			}
-			else {
-				markersPlane[the_id].setPosition(point);
-			}
-
-			//alert(data);
-		};
-
+}).addTo(map);
+			};
 		// close
 		wsPlane.onclose = function() {
 		};
 
 	} else {
 		alert("Your broswer does not support WebSockets. Try using the latest Firefox, Chrome or Safari browser.");
-
 	};
-
 }
